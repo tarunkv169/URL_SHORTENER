81613a196fb84661e10bb583d7f46107e980b387
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-06-21 23:26:22 +0530

    url handling functions

---
+++ controller/url.js
@@ -0,0 +1,76 @@
+const URL = require('../models/url');
+const shortid = require('shortid');
+
+
+
+async function handleShortId(req, res) {
+    const body = req.body;
+    if (!body.url) {
+        return res.status(400).json({ error: "URL is required" });
+    }
+
+    const shortId = shortid.generate();
+    await URL.create({
+        shortId: shortId,
+        redirectUrl: body.url,
+        visitHistory: []
+    });
+
+    // return res.json({ id: shortId });
+    
+    const allUrls = await URL.find({}); // Fetch all URLs to include in the response
+    return res.render("home", { id: shortId, urls: allUrls });
+}
+
+
+
+
+
+async function handleRedirectToUrl(req, res) {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        { shortId },
+        { $push: { visitHistory: { timestamp: Date.now() } } },
+        { new: true }
+    );
+
+    if (!entry) {
+        return res.status(404).json({ error: "URL not found" });
+    }
+
+    res.redirect(entry.redirectUrl);
+}
+
+
+
+
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+
+    if (!result) {
+        return res.status(404).json({ error: "URL not found" });
+    }
+
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory
+    });
+}
+
+
+async function handleAllUsersURL(req,res){
+    const allurls = await URL.find({});
+    console.log(allurls)
+    return res.render("home",{ id:null, urls: allurls  });     // SSR on file("home", u can give data here to render out of file)
+                                                              //                        ^---data send from here(from backend) will save in "locals"(which we can extract in ejs file)
+}
+
+
+module.exports = {
+    handleShortId,
+    handleRedirectToUrl,
+    handleGetAnalytics,
+    handleAllUsersURL
+};
