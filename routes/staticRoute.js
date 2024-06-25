108db66a72f7b4b2b432db3a693487ee273e6297
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-06-25 14:50:28 +0530

    authorization middleware to routes

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {renderAllURL,renderSignUp,renderLogin} = require("../controller/staticControl");
+const {renderAllURL,renderParticularUserURL,renderSignUp,renderLogin} = require("../controller/staticControl");
+const { restrictToRoles } = require("../middleware/auth");
 
 // Route to render the form and display all URLs
-   router.get('/',renderAllURL);
+   router.get('/admin/urls',restrictToRoles(["ADMIN"]),renderAllURL);
+
+   router.get('/',restrictToRoles(["NORMAL","ADMIN"]),renderParticularUserURL);      // 🛑here we give "urls" of particular "user",,so need of "middleware" of particular "role" so go to 🛑"Staticrouter"
 
    router.get('/signup',renderSignUp)
    router.get('/login',renderLogin)
